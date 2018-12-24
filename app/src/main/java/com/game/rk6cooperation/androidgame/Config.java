4984fcdf1079dcd8d14ab2014e53e3f19f083312
Tim <garif-tim@yandex.ru>
2018-12-25 01:55:54 +0300

    add sounds for number buttons

--- app/src/main/java/com/game/rk6cooperation/androidgame/Config.java
+++ app/src/main/java/com/game/rk6cooperation/androidgame/Config.java
@@ -4,4 +4,17 @@ public class Config {
     public static final String BASE_URL = "http://130.193.43.124:3220";
     public final static String KEY_SESSION = "session";
     public final static String KEY_NICKNAME = "nickname";
+
+    public final static String SOUND_ZERO = "010.ogg";
+    public final static String SOUND_ONE = "001.ogg";
+    public final static String SOUND_TWO = "002.ogg";
+    public final static String SOUND_THREE = "003.ogg";
+    public final static String SOUND_FOUR = "004.ogg";
+    public final static String SOUND_FIVE = "005.ogg";
+    public final static String SOUND_SIX = "006.ogg";
+    public final static String SOUND_SEVEN = "007.ogg";
+    public final static String SOUND_EIGHT = "008.ogg";
+    public final static String SOUND_NINE = "009.ogg";
+    public final static String SOUND_11 = "011.ogg";
+    public final static String SOUND_12 = "012.ogg";
 }
--- app/src/main/java/com/game/rk6cooperation/androidgame/GameActivity.java
+++ app/src/main/java/com/game/rk6cooperation/androidgame/GameActivity.java
@@ -1,6 +1,13 @@
 package com.game.rk6cooperation.androidgame;
 
 import android.animation.ObjectAnimator;
+import android.annotation.TargetApi;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -9,9 +16,11 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.game.rk6cooperation.androidgame.Network.WorkerThread;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -21,6 +30,7 @@ import java.util.concurrent.Executors;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -30,6 +40,13 @@ public class GameActivity extends AppCompatActivity {
     private final List<String> availableNumbers = Arrays.asList(
             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "#"
     );
+    private SoundPool mSoundPool;
+    private AssetManager mAssetManager;
+    private int mOneButSound, mTwoButSound, mThreeButSound, mFourButSound;
+    private int mFiveButSound, mSixButSound, mSevenButSound,  mEightButSound;
+    private int mNineButSound, mZeroButSound, m11ButSound, m12ButSound;
+    private int mStreamID;
+
     @BindView(R.id.game_ground_container)
     LinearLayout gameGroundContainer;
     @BindView(R.id.button0)
@@ -135,6 +152,42 @@ public class GameActivity extends AppCompatActivity {
         super.onStop();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            createOldSoundPool();
+        } else {
+            createNewSoundPool();
+        }
+
+        mAssetManager = getAssets();
+
+        mZeroButSound = loadSound(Config.SOUND_ZERO);
+        mOneButSound = loadSound(Config.SOUND_ONE);
+        mTwoButSound = loadSound(Config.SOUND_TWO);
+        mThreeButSound = loadSound(Config.SOUND_THREE);
+        mFourButSound = loadSound(Config.SOUND_FOUR);
+        mFiveButSound = loadSound(Config.SOUND_FIVE);
+        mSixButSound = loadSound(Config.SOUND_SIX);
+        mSevenButSound = loadSound(Config.SOUND_SEVEN);
+        mEightButSound = loadSound(Config.SOUND_EIGHT);
+        mNineButSound = loadSound(Config.SOUND_NINE);
+        m11ButSound = loadSound(Config.SOUND_11);
+        m12ButSound = loadSound(Config.SOUND_12);
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSoundPool.release();
+        mSoundPool = null;
+    }
+
+
     private void generateNumbers() {
         Integer availableNumbersSize = availableNumbers.size();
         while (true) {
@@ -183,6 +236,104 @@ public class GameActivity extends AppCompatActivity {
             moveValue += 10;
         }
     }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void createNewSoundPool() {
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+        mSoundPool = new SoundPool.Builder()
+                .setAudioAttributes(attributes)
+                .build();
+    }
+
+    private void createOldSoundPool() {
+        mSoundPool = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
+    }
+
+    private int playSound(int sound) {
+        if (sound > 0) {
+            mStreamID = mSoundPool.play(sound, 1, 1, 1, 0, 1);
+        }
+        return mStreamID;
+    }
+
+    private int loadSound(String fileName) {
+        AssetFileDescriptor afd;
+        try {
+            afd = mAssetManager.openFd(fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Can't load: " + fileName,
+                    Toast.LENGTH_SHORT).show();
+            return -1;
+        }
+        return mSoundPool.load(afd, 1);
+    }
+
+
+    @OnClick(R.id.button0)
+    void onClickZero() {
+        playSound(mZeroButSound);
+    }
+
+    @OnClick(R.id.button1)
+    void onClickOne() {
+        playSound(mOneButSound);
+    }
+
+    @OnClick(R.id.button2)
+    void onClickTwo() {
+        playSound(mTwoButSound);
+    }
+
+    @OnClick(R.id.button3)
+    void onClickThree() {
+        playSound(mThreeButSound);
+    }
+
+    @OnClick(R.id.button4)
+    void onClickFour() {
+        playSound(mFourButSound);
+    }
+
+    @OnClick(R.id.button5)
+    void onClickFive() {
+        playSound(mFiveButSound);
+    }
+
+    @OnClick(R.id.button6)
+    void onClickSix() {
+        playSound(mSixButSound);
+    }
+
+    @OnClick(R.id.button7)
+    void onClickSeven() {
+        playSound(mSevenButSound);
+    }
+
+    @OnClick(R.id.button8)
+    void onClickEight() {
+        playSound(mEightButSound);
+    }
+
+    @OnClick(R.id.button9)
+    void onClickNine() {
+        playSound(mNineButSound);
+    }
+
+    @OnClick(R.id.button_hashtag)
+    void onClick11() {
+        playSound(m11ButSound);
+    }
+
+    @OnClick(R.id.button_star)
+    void onClick12() {
+        playSound(m12ButSound);
+    }
+
+
 
 }
 
